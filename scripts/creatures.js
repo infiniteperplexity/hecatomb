f0731bdd0ff0642a30b54e2e4dc9d8252867340a
Glenn <infinite.perplexity@gmail.com>
2017-08-10 11:00:24 -0700

    minor tweak

--- scripts/creatures.js
+++ scripts/creatures.js
@@ -16,7 +16,7 @@ HTomb = (function(HTomb) {
         AI: {
           team: "PlayerTeam"
         },
-        Master: {tasks: ["DigTask","BuildTask","DismantleTask","PatrolTask","Undesignate","HostileTask"]},
+        Master: {tasks: ["DigTask","BuildTask","ConstructTask","DismantleTask","PatrolTask","FurnishTask","Undesignate","HostileTask"]},
         //Master: {tasks: ["DigTask","BuildTask","ConstructTask","DismantleTask","PatrolTask","FurnishTask","ForbidTask","Undesignate"]},
         //Master: {tasks: ["DigTask","BuildTask","CraftTask","DismantleTask","PatrolTask","FarmTask","WorkshopTask","ForbidTask","HoardTask","Undesignate"]},
         //SpellCaster: {spells: ["RaiseZombie","AcidBolt","ParticleTest"]},
