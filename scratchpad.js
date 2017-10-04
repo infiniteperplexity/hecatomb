12cab91b7feeccf9ba5061a290ac3ebaef865e23
infiniteperplexity <Glenn.T.Wright@kp.org>
2017-10-04 15:59:01 -0700

    trying a thing to get rid of security warnings

--- scratchpad.js
+++ scratchpad.js
@@ -5,21 +5,16 @@ Behaviors.extend({
   name: "craftable",
   labor: 10,
   ingredients: {},
-  onSetUp: function() {
-    for (let t in HTomb.Things.templates) {
-      let template = HTomb.Things.templates[t];
-      if (template.Behaviors && template.Behaviors.Craftable) {
-        let item = HTomb.Things.templates.Item.extend({
-          template: template.template+"Item",
-          name: template.name
-        });
-        item.fg = template.fg || "white";
-        item.symbol = template.symbol || "?";
-        item.ingredients = template.Behaviors.Craftable.ingredients || this.ingredients;
-        item.labor = template.Behaviors.Craftable.labor || this.labor;
-        template.ingredients = {};
-        template.ingredients[template.template+"Item"] = 1;
-      }
-    }
+  dismantle: function() {
+
   }
 });
+
+Behaviors.extend({
+  template: "Harvestable",
+  name: "harvestable",
+  yields: {},
+  harvest: function() {
+
+  }
+})
