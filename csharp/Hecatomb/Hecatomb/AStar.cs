1c37d7879998b1019e59e1cef702812e2ae49b87
infiniteperplexity <infinite.perplexity@gmail.com>
2018-09-23 11:54:28 -0700

    it sometimes works now!

--- csharp/Hecatomb/Hecatomb/AStar.cs
+++ csharp/Hecatomb/Hecatomb/AStar.cs
@@ -21,7 +21,6 @@ namespace Hecatomb
 		{
 			if (x1<0 || x1>=Constants.WIDTH || y1<0 || y1>=Constants.HEIGHT || z1<0 || z1>=Constants.DEPTH)
 			{
-	/// </summary>
       			return false;
     		}
 			Terrain t = Game.World.Tiles[x1,y1,z1];
@@ -32,7 +31,6 @@ namespace Hecatomb
 		{
 			if (x1<0 || x1>=Constants.WIDTH || y1<0 || y1>=Constants.HEIGHT || z1<0 || z1>=Constants.DEPTH)
 			{
-	/// </summary>
       			return false;
     		}
 			Terrain t = Game.World.Tiles[x1,y1,z1];
@@ -44,6 +42,7 @@ namespace Hecatomb
 			Func<int, int, int, int, int, int, bool> movable = null,
 			Func<int, int, int, bool> passable = null
 		) {
+			Debug.WriteLine("Finding path from {0} {1} {2} to {3} {4} {5}",x0,y0,z0,x1,y1,z1);
 			// default value for the cost estimation heuristic
 			heuristic = heuristic ?? QuickDistance;
 			// default value for allowable movement
@@ -79,19 +78,30 @@ namespace Hecatomb
 				queue.RemoveFirst();
 				// ***** if we found the goal, retrace our steps ****
 				if (current.x==x1 && current.y==y1 && current.z==z1) {
+					Debug.WriteLine("retracing path");
 					// ***trace backwards
 					Coord previous = current;
+					int j = 0;
 					while (retrace.ContainsKey(current))
-					{
-					       	previous = retrace[current];
-					       	if (retrace.ContainsKey(previous)) {
-					       		current = previous;
-					       	}
+					{		
+						j+=1;
+						if (j>=10) {
+							return null;
+						}
+						previous = retrace[current];
+					    if (retrace.ContainsKey(previous)) {
+					       	current = previous;
+						} else {
+//							if (useFirst
+							Debug.Print("Should step to {0} {1} {2}", current.x, current.y, current.z);
+							return current;
+						}
 					}
 					return current;
 				}
 				// ***************************************************
 				Coord neighbor;
+				LinkedListNode<Coord> node;
 				// ***** check passability and scores for neighbors **
 				foreach (Coord dir in dirs)
 				{
@@ -101,29 +111,29 @@ namespace Hecatomb
 						{
 							// cost of taking this step
 							cost = 1;
-							// actual score along this path						
+							// actual score along this path	
+							// !!! somehow I ran into a situation where this key was missing?							
 							newScore = gscores[current] + cost;
 							// if this is the best known path to the cell...
 							if (!gscores.ContainsKey(neighbor) || gscores[neighbor] > newScore) {
 								fscore = newScore + heuristic(neighbor.x, neighbor.y, neighbor.z, x1, y1, z1);
-								if (!fscores.ContainsKey(neighbor) || fscores[neighbor]>=fscore)
+								fscores[neighbor] = fscore;
+								// loop through the queue to insert in sorted order
+								node = queue.First;
+								if (node==null) {
+									queue.AddFirst(neighbor);
+								} else while (node!=null)
 								{
-									fscores[neighbor] = fscore;
-									// loop through the queue to insert in sorted order
-									LinkedListNode<Coord> node = queue.First;
-									while (node!=null)
+									if (fscores[node.Value]>fscore)
 									{
-										if (fscores[node.Value]>fscore)
+										queue.AddBefore(node, neighbor);
+										node = null;
+									} else {
+										node = node.Next;
+										// if we made it all the way to the end, insert it there
+										if (node==null)
 										{
-											queue.AddBefore(node, neighbor);
-											node = null;
-										} else {
-											node = node.Next;
-											// if we made it all the way to the end, insert it there
-											if (node==null)
-											{
-												queue.AddLast(neighbor);
-											}
+											queue.AddLast(neighbor);
 										}
 									}
 									retrace[neighbor] = current;
