3926f04cc973eddc6f925bedc6f8aaa17fa59031
infiniteperplexity <infinite.perplexity@gmail.com>
2018-09-23 12:27:28 -0700

    scavenged some MonoGame code

---
+++ csharp/Hecatomb/Hecatomb/scavenged/Game1.cs
@@ -0,0 +1,236 @@
+﻿#region Using Statements
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using RogueSharp;
+using RogueSharp.DiceNotation;
+
+#endregion
+
+namespace ExampleGame
+{
+   /// <summary>
+   /// This is the main type for your game
+   /// </summary>
+   public class Game1 : Game
+   {
+      GraphicsDeviceManager graphics;
+      SpriteBatch spriteBatch;
+
+      private Texture2D _floor;
+      private Texture2D _wall;
+      private IMap _map;
+      private Player _player;
+      private List<AggressiveEnemy> _aggressiveEnemies = new List<AggressiveEnemy>();
+      private InputState _inputState;
+
+      public Game1()
+         : base()
+      {
+         graphics = new GraphicsDeviceManager( this );
+         Content.RootDirectory = "Content";
+         _inputState = new InputState();
+      }
+
+      /// <summary>
+      /// Allows the game to perform any initialization it needs to before starting to run.
+      /// This is where it can query for any required services and load any non-graphic
+      /// related content.  Calling base.Initialize will enumerate through any components
+      /// and initialize them as well.
+      /// </summary>
+      protected override void Initialize()
+      {
+         // TODO: Add your initialization logic here
+         Global.Camera.ViewportWidth = graphics.GraphicsDevice.Viewport.Width;
+         Global.Camera.ViewportHeight = graphics.GraphicsDevice.Viewport.Height;
+         IMapCreationStrategy<Map> mapCreationStrategy = new RandomRoomsMapCreationStrategy<Map>( Global.MapWidth, Global.MapHeight, 100, 7, 3 );
+         _map = Map.Create( mapCreationStrategy );
+
+         base.Initialize();
+      }
+
+      /// <summary>
+      /// LoadContent will be called once per game and is the place to load
+      /// all of your content.
+      /// </summary>
+      protected override void LoadContent()
+      {
+         // Create a new SpriteBatch, which can be used to draw textures.
+         spriteBatch = new SpriteBatch( GraphicsDevice );
+
+         // TODO: use this.Content to load your game content here
+         _floor = Content.Load<Texture2D>( "Floor" );
+         _wall = Content.Load<Texture2D>( "Wall" );
+         Cell startingCell = GetRandomEmptyCell();
+         _player = new Player
+         {
+            X = startingCell.X,
+            Y = startingCell.Y,
+            Sprite = Content.Load<Texture2D>( "Player" ),
+            ArmorClass = 15,
+            AttackBonus = 1,
+            Damage = Dice.Parse( "2d4" ),
+            Health = 50,
+            Name = "Mr. Rogue"
+         };
+         UpdatePlayerFieldOfView();
+         Global.Camera.CenterOn( startingCell );
+
+         AddAggressiveEnemies( 10 );
+         Global.CombatManager = new CombatManager( _player, _aggressiveEnemies );
+
+         Global.GameState = GameStates.PlayerTurn;
+      }
+
+      /// <summary>
+      /// UnloadContent will be called once per game and is the place to unload
+      /// all content.
+      /// </summary>
+      protected override void UnloadContent()
+      {
+         // TODO: Unload any non ContentManager content here
+      }
+
+      /// <summary>
+      /// Allows the game to run logic such as updating the world,
+      /// checking for collisions, gathering input, and playing audio.
+      /// </summary>
+      /// <param name="gameTime">Provides a snapshot of timing values.</param>
+      protected override void Update( GameTime gameTime )
+      {
+         // TODO: Add your update logic here
+         _inputState.Update();
+         Global.Camera.HandleInput( _inputState, PlayerIndex.One );
+         if ( _inputState.IsExitGame( PlayerIndex.One ) )
+         {
+            Exit();
+         }
+         else if ( _inputState.IsSpace( PlayerIndex.One ) )
+         {
+            if ( Global.GameState == GameStates.PlayerTurn )
+            {
+               Global.GameState = GameStates.Debugging;
+            }
+            else if ( Global.GameState == GameStates.Debugging )
+            {
+               Global.GameState = GameStates.PlayerTurn;
+            }
+         }
+         else
+         {
+            if ( Global.GameState == GameStates.PlayerTurn 
+               && _player.HandleInput( _inputState, _map ) )
+            {
+               UpdatePlayerFieldOfView();
+               Global.Camera.CenterOn( _map.GetCell( _player.X, _player.Y ) );
+               Global.GameState = GameStates.EnemyTurn;
+            }
+            if ( Global.GameState == GameStates.EnemyTurn )
+            {
+               foreach ( var enemy in _aggressiveEnemies )
+               {
+                  enemy.Update();
+               }
+               Global.GameState = GameStates.PlayerTurn;
+            }
+         }
+
+         base.Update( gameTime );
+      }
+
+      /// <summary>
+      /// This is called when the game should draw itself.
+      /// </summary>
+      /// <param name="gameTime">Provides a snapshot of timing values.</param>
+      protected override void Draw( GameTime gameTime )
+      {
+         GraphicsDevice.Clear( Color.Black );
+
+         // TODO: Add your drawing code here
+         spriteBatch.Begin( SpriteSortMode.BackToFront, BlendState.AlphaBlend, null, null, null, null, Global.Camera.TranslationMatrix );
+
+         foreach ( Cell cell in _map.GetAllCells() )
+         {
+            var position = new Vector2( cell.X * Global.SpriteWidth, cell.Y * Global.SpriteHeight );
+            if ( !cell.IsExplored && Global.GameState != GameStates.Debugging )
+            {
+               continue;
+            }
+            Color tint = Color.White;
+            if ( !cell.IsInFov && Global.GameState != GameStates.Debugging )
+            {
+               tint = Color.Gray;
+            }
+            if ( cell.IsWalkable )
+            {
+               spriteBatch.Draw( _floor, position, null, null, null, 0.0f, Vector2.One, tint, SpriteEffects.None, LayerDepth.Cells );
+            }
+            else
+            {
+               spriteBatch.Draw( _wall, position, null, null, null, 0.0f, Vector2.One, tint, SpriteEffects.None, LayerDepth.Cells );
+            }
+         }
+
+         _player.Draw( spriteBatch );
+
+         foreach ( var enemy in _aggressiveEnemies )
+         {
+            if ( Global.GameState == GameStates.Debugging || _map.IsInFov( enemy.X, enemy.Y ) )
+            {
+               enemy.Draw( spriteBatch );
+            }
+         }
+
+         spriteBatch.End();
+
+         base.Draw( gameTime );
+      }
+
+      private void UpdatePlayerFieldOfView()
+      {
+         _map.ComputeFov( _player.X, _player.Y, 30, true );
+         foreach ( Cell cell in _map.GetAllCells() )
+         {
+            if ( _map.IsInFov( cell.X, cell.Y ) )
+            {
+               _map.SetCellProperties( cell.X, cell.Y, cell.IsTransparent, cell.IsWalkable, true );
+            }
+         }
+      }
+
+      private void AddAggressiveEnemies( int numberOfEnemies )
+      {
+         for ( int i = 0; i < numberOfEnemies; i++ )
+         {
+            Cell enemyCell = GetRandomEmptyCell();
+            var pathFromAggressiveEnemy = new PathToPlayer( _player, _map, Content.Load<Texture2D>( "White" ) );
+            pathFromAggressiveEnemy.CreateFrom( enemyCell.X, enemyCell.Y );
+
+            var enemy = new AggressiveEnemy( _map, pathFromAggressiveEnemy ) {
+               X = enemyCell.X,
+               Y = enemyCell.Y,
+               Sprite = Content.Load<Texture2D>( "Hound" ),
+               ArmorClass = 10,
+               AttackBonus = 0,
+               Damage = Dice.Parse( "d3" ),
+               Health = 10,
+               Name = "Hunting Hound"
+            };
+            _aggressiveEnemies.Add( enemy );
+         }
+      }
+
+      private Cell GetRandomEmptyCell()
+      {
+         while ( true )
+         {
+            int x = Global.Random.Next( 49 );
+            int y = Global.Random.Next( 29 );
+            if ( _map.IsWalkable( x, y ) )
+            {
+               return _map.GetCell( x, y );
+            }
+         }
+      }
+   }
+}
---
+++ csharp/Hecatomb/Hecatomb/scavenged/InputState.cs
@@ -0,0 +1,282 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// InputState.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace ExampleGame
+{
+   /// <summary>
+   ///    Helper for reading input from keyboard, gamepad, and touch input. This class
+   ///    tracks both the current and previous state of the input devices, and implements
+   ///    query methods for high level input actions such as "move up through the menu"
+   ///    or "pause the game".
+   /// </summary>
+   public class InputState
+   {
+      public const int MaxInputs = 4;
+
+      public readonly GamePadState[] CurrentGamePadStates;
+      public readonly KeyboardState[] CurrentKeyboardStates;
+      public readonly bool[] GamePadWasConnected;
+
+      public readonly GamePadState[] LastGamePadStates;
+      public readonly KeyboardState[] LastKeyboardStates;
+
+      public InputState()
+      {
+         CurrentKeyboardStates = new KeyboardState[MaxInputs];
+         CurrentGamePadStates = new GamePadState[MaxInputs];
+
+         LastKeyboardStates = new KeyboardState[MaxInputs];
+         LastGamePadStates = new GamePadState[MaxInputs];
+
+         CurrentMouseState = new MouseState();
+         LastMouseState = new MouseState();
+
+         GamePadWasConnected = new bool[MaxInputs];
+      }
+
+      public MouseState CurrentMouseState
+      {
+         get;
+         private set;
+      }
+
+      public MouseState LastMouseState
+      {
+         get;
+         private set;
+      }
+
+      /// <summary>
+      ///    Reads the latest state of the keyboard and gamepad.
+      /// </summary>
+      public void Update()
+      {
+         for ( int i = 0; i < MaxInputs; i++ )
+         {
+            LastKeyboardStates[i] = CurrentKeyboardStates[i];
+            LastGamePadStates[i] = CurrentGamePadStates[i];
+
+            CurrentKeyboardStates[i] = Keyboard.GetState( (PlayerIndex) i );
+            CurrentGamePadStates[i] = GamePad.GetState( (PlayerIndex) i );
+
+            // Keep track of whether a gamepad has ever been
+            // connected, so we can detect if it is unplugged.
+            if ( CurrentGamePadStates[i].IsConnected )
+            {
+               GamePadWasConnected[i] = true;
+            }
+         }
+
+         LastMouseState = CurrentMouseState;
+         CurrentMouseState = Mouse.GetState();
+      }
+
+      public bool IsNewLeftMouseClick( out MouseState mouseState )
+      {
+         mouseState = CurrentMouseState;
+         return ( CurrentMouseState.LeftButton == ButtonState.Released && LastMouseState.LeftButton == ButtonState.Pressed );
+      }
+
+      public bool IsNewRightMouseClick( out MouseState mouseState )
+      {
+         mouseState = CurrentMouseState;
+         return ( CurrentMouseState.RightButton == ButtonState.Released && LastMouseState.RightButton == ButtonState.Pressed );
+      }
+
+      public bool IsNewThirdMouseClick( out MouseState mouseState )
+      {
+         mouseState = CurrentMouseState;
+         return ( CurrentMouseState.MiddleButton == ButtonState.Pressed && LastMouseState.MiddleButton == ButtonState.Released );
+      }
+
+      public bool IsNewMouseScrollUp( out MouseState mouseState )
+      {
+         mouseState = CurrentMouseState;
+         return ( CurrentMouseState.ScrollWheelValue > LastMouseState.ScrollWheelValue );
+      }
+
+      public bool IsNewMouseScrollDown( out MouseState mouseState )
+      {
+         mouseState = CurrentMouseState;
+         return ( CurrentMouseState.ScrollWheelValue < LastMouseState.ScrollWheelValue );
+      }
+
+      /// <summary>
+      ///    Helper for checking if a key was newly pressed during this update. The
+      ///    controllingPlayer parameter specifies which player to read input for.
+      ///    If this is null, it will accept input from any player. When a keypress
+      ///    is detected, the output playerIndex reports which player pressed it.
+      /// </summary>
+      public bool IsNewKeyPress( Keys key, PlayerIndex? controllingPlayer, out PlayerIndex playerIndex )
+      {
+         if ( controllingPlayer.HasValue )
+         {
+            // Read input from the specified player.
+            playerIndex = controllingPlayer.Value;
+
+            var i = (int) playerIndex;
+
+            return ( CurrentKeyboardStates[i].IsKeyDown( key ) && LastKeyboardStates[i].IsKeyUp( key ) );
+         }
+         else
+         {
+            // Accept input from any player.
+            return ( IsNewKeyPress( key, PlayerIndex.One, out playerIndex ) || IsNewKeyPress( key, PlayerIndex.Two, out playerIndex )
+                     || IsNewKeyPress( key, PlayerIndex.Three, out playerIndex ) || IsNewKeyPress( key, PlayerIndex.Four, out playerIndex ) );
+         }
+      }
+
+      /// <summary>
+      ///    Helper for checking if a button was newly pressed during this update.
+      ///    The controllingPlayer parameter specifies which player to read input for.
+      ///    If this is null, it will accept input from any player. When a button press
+      ///    is detected, the output playerIndex reports which player pressed it.
+      /// </summary>
+      public bool IsNewButtonPress( Buttons button, PlayerIndex? controllingPlayer, out PlayerIndex playerIndex )
+      {
+         if ( controllingPlayer.HasValue )
+         {
+            // Read input from the specified player.
+            playerIndex = controllingPlayer.Value;
+
+            var i = (int) playerIndex;
+
+            return ( CurrentGamePadStates[i].IsButtonDown( button ) && LastGamePadStates[i].IsButtonUp( button ) );
+         }
+         else
+         {
+            // Accept input from any player.
+            return ( IsNewButtonPress( button, PlayerIndex.One, out playerIndex ) || IsNewButtonPress( button, PlayerIndex.Two, out playerIndex )
+                     || IsNewButtonPress( button, PlayerIndex.Three, out playerIndex ) || IsNewButtonPress( button, PlayerIndex.Four, out playerIndex ) );
+         }
+      }
+
+      public bool IsKeyPressed( Keys key, PlayerIndex? controllingPlayer, out PlayerIndex playerIndex )
+      {
+         if ( controllingPlayer.HasValue )
+         {
+            // Read input from the specified player.
+            playerIndex = controllingPlayer.Value;
+
+            var i = (int) playerIndex;
+
+            return ( CurrentKeyboardStates[i].IsKeyDown( key ) );
+         }
+         else
+         {
+            // Accept input from any player.
+            return ( IsKeyPressed( key, PlayerIndex.One, out playerIndex ) || IsKeyPressed( key, PlayerIndex.Two, out playerIndex )
+                     || IsKeyPressed( key, PlayerIndex.Three, out playerIndex ) || IsKeyPressed( key, PlayerIndex.Four, out playerIndex ) );
+         }
+      }
+
+      public bool IsButtonPressed( Buttons button, PlayerIndex? controllingPlayer, out PlayerIndex playerIndex )
+      {
+         if ( controllingPlayer.HasValue )
+         {
+            // Read input from the specified player.
+            playerIndex = controllingPlayer.Value;
+
+            var i = (int) playerIndex;
+
+            return ( CurrentGamePadStates[i].IsButtonDown( button ) );
+         }
+         else
+         {
+            // Accept input from any player.
+            return ( IsButtonPressed( button, PlayerIndex.One, out playerIndex ) || IsButtonPressed( button, PlayerIndex.Two, out playerIndex )
+                     || IsButtonPressed( button, PlayerIndex.Three, out playerIndex ) || IsButtonPressed( button, PlayerIndex.Four, out playerIndex ) );
+         }
+      }
+
+      public bool IsExitGame( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+
+         return IsNewKeyPress( Keys.Escape, controllingPlayer, out playerIndex ) || IsNewButtonPress( Buttons.Back, controllingPlayer, out playerIndex );
+      }
+
+      public bool IsLeft( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+
+         return IsNewKeyPress( Keys.Left, controllingPlayer, out playerIndex ) || IsNewButtonPress( Buttons.DPadLeft, controllingPlayer, out playerIndex )
+                || IsNewButtonPress( Buttons.LeftThumbstickLeft, controllingPlayer, out playerIndex );
+      }
+
+      public bool IsRight( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+
+         return IsNewKeyPress( Keys.Right, controllingPlayer, out playerIndex ) || IsNewButtonPress( Buttons.DPadRight, controllingPlayer, out playerIndex )
+                || IsNewButtonPress( Buttons.LeftThumbstickRight, controllingPlayer, out playerIndex );
+      }
+
+      public bool IsUp( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+
+         return IsNewKeyPress( Keys.Up, controllingPlayer, out playerIndex ) || IsNewButtonPress( Buttons.DPadUp, controllingPlayer, out playerIndex )
+                || IsNewButtonPress( Buttons.LeftThumbstickUp, controllingPlayer, out playerIndex );
+      }
+
+      public bool IsDown( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+
+         return IsNewKeyPress( Keys.Down, controllingPlayer, out playerIndex ) || IsNewButtonPress( Buttons.DPadDown, controllingPlayer, out playerIndex )
+                || IsNewButtonPress( Buttons.LeftThumbstickDown, controllingPlayer, out playerIndex );
+      }
+
+      public bool IsSpace( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+         return IsNewKeyPress( Keys.Space, controllingPlayer, out playerIndex );
+      }
+
+      public bool IsScrollLeft( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+         return IsKeyPressed( Keys.A, controllingPlayer, out playerIndex );
+      }
+
+      public bool IsScrollRight( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+         return IsKeyPressed( Keys.D, controllingPlayer, out playerIndex );
+      }
+
+      public bool IsScrollUp( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+         return IsKeyPressed( Keys.W, controllingPlayer, out playerIndex );
+      }
+
+      public bool IsScrollDown( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+         return IsKeyPressed( Keys.S, controllingPlayer, out playerIndex );
+      }
+
+      public bool IsZoomOut( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+         return IsNewKeyPress( Keys.OemPeriod, controllingPlayer, out playerIndex );
+      }
+
+      public bool IsZoomIn( PlayerIndex? controllingPlayer )
+      {
+         PlayerIndex playerIndex;
+         return IsNewKeyPress( Keys.OemComma, controllingPlayer, out playerIndex );
+      }
+   }
+}
