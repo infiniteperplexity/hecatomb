1aecc24b96fef35342cf1a5bd5c1748a40a53394
infiniteperplexity <infinite.perplexity@gmail.com>
2019-05-24 10:43:07 -0700

    dwarf lairs are a go

--- csharp/Hecatomb/Hecatomb/LairHandler.cs
+++ csharp/Hecatomb/Hecatomb/LairHandler.cs
@@ -15,44 +15,96 @@ namespace Hecatomb
 
         public bool FindHillSide()
         {
-            // pick a random spot along the north wall for now
-            int y = Game.World.Random.Next(25) + 12;
-            int x = Game.World.Random.Next(100) + 75;
+            // pick a random spot nearish to the edge of the map
+            int side = Game.World.Random.Next(4);
+            int x, y;
+            if (side == 0)
+            {
+                x = 25;
+                y = Game.World.Random.Next(1, 254);
+            }
+            else if (side == 1)
+            {
+                x = 230;
+                y = Game.World.Random.Next(1, 254);
+            }
+            else if (side == 2)
+            {
+                y = 25;
+                x = Game.World.Random.Next(1, 254);
+            }
+            else
+            {
+                y = 230;
+                x = Game.World.Random.Next(1, 254);
+            }
             int z = Game.World.GetGroundLevel(x, y);
             if (Game.World.Covers[x, y, z].Liquid)
             {
                 return false;
             }
+
+            int dir = Game.World.Random.Next(4);
+            int dx, dy;
+            if (dir == 0)
+            {
+                dx = 1;
+                dy = 0;
+            }
+            else if (dir == 1)
+            {
+                dx = -1;
+                dy = 0;
+            }
+            else if (dir == 2)
+            {
+                dx = 0;
+                dy = 1;
+            }
+            else
+            {
+                dx = 0;
+                dy = -1;
+            }
+
             int maxtries = 25;
             // then drive southward looking for a hillside
             for (int i = 0; i < maxtries; i++)
             {
-                if (Game.World.Terrains[x, y + i, z].Solid)
+                if (Game.World.Terrains[x + i * dx, y + i * dy, z].Solid)
                 {
-                    if (CheckBoundingBox(x, y + i, z))
+                    int hallwayLength = 3;
+                    int setPieceRadius = 6;
+                    int x0 = x + (i + hallwayLength + setPieceRadius - 1) * dx;
+                    int y0 = y + (i + hallwayLength + setPieceRadius - 1) * dy;
+                    if (CheckBoundingBox(x0, y0, z, dx, dy))
                     {
-                        BuildDwarfLair(x, y + i, z);
+                        BuildDwarfLair(x0, y0, z, dx, dy);
                         return true;
                     }
                     else
                     {
-                        continue;
+                        return false;
                     }
                 }
             }
             return false;
         }
 
-        // always south for now
-        public bool CheckBoundingBox(int x, int y, int z)
+        public bool CheckBoundingBox(int x0, int y0, int z, int dx, int dy)
         {
-            int hallwayLength = 3;
-            for (int i = -6; i <= +6; i++)
+            int setPieceRadius = 6;
+            for (int i = -setPieceRadius; i <= +setPieceRadius ; i++)
             {
-                for (int j = 0; j < 13; j++)
+                for (int j = -setPieceRadius; j <= +setPieceRadius; j++)
                 {
-                    // need to check world bounds too
-                    if (!Game.World.Terrains[x + i, y + j + hallwayLength - 1, z].Solid)
+                    // out of bounds
+                    if (x0 + i < 1 || x0 + i > Game.World.Width - 2 || y0 + j < 1 || y0 + j > Game.World.Width - 2)
+                    {
+                        return false;
+                    }
+                    // not solid rock
+                    if (!Game.World.Terrains[x0 + i, y0 + j, z].Solid)
                     {
                         return false;
                     }
@@ -61,32 +113,64 @@ namespace Hecatomb
             return true;
         }
 
-        public void BuildDwarfLair(int x, int y, int z)
+        public void BuildDwarfLair(int x0, int y0, int z, int dx, int dy)
         {
-            Debug.WriteLine($"building a dwarf lair south of {x} {y} {z}");
+            Debug.WriteLine($"building a dwarf lair centered on {x0} {y0} {z}");
             int hallwayLength = 3;
+            int setPieceRadius = 6;
+            // the top left corner
+            int x1 = x0 - setPieceRadius;
+            int y1 = y0 - setPieceRadius;
             for (int i = 0; i < hallwayLength; i++)
             {
-                Game.World.Terrains[x, y + i, z] = Terrain.FloorTile;
-                Game.World.Covers[x, y + i, z] = Cover.NoCover;
+                Game.World.Terrains[x0 - (setPieceRadius + i) * dx, y0 - (setPieceRadius + i) * dy, z] = Terrain.FloorTile;
+                Game.World.Covers[x0 - (setPieceRadius + i) * dx, y0 - (setPieceRadius + i) * dy, z] = Cover.NoCover;
             }
-            for (int i = -5; i <= +5; i++)
+            for (int i = 1; i < 12; i++)
             {
                 for (int j = 1; j < 12; j++)
                 {
-                    Game.World.Terrains[x + i, y + j + hallwayLength - 1, z] = Terrain.FloorTile;
-                    Game.World.Covers[x + i, y + j + hallwayLength - 1, z] = Cover.NoCover;
+                    // wall it off into rooms
+                    if (i % 4 != 0 && j % 4 != 0)
+                    {
+                        Game.World.Terrains[x1 + i, y1 + j, z] = Terrain.FloorTile;
+                        Game.World.Covers[x1 + i, y1 + j, z] = Cover.NoCover;
+                    }
                 }
             }
+            // dig holes in the walls to connect rooms
+            var maze = Maze.Generate(3, 3);
+            // it seems like the mazes must be bad
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    bool right = maze[i, j, 0];
+                    bool down = maze[i, j, 1];
+                    if (i < 2 && !right)
+                    {
+                        Game.World.Terrains[x1 + (i + 1) * 4, y1 + (j + 1) * 4 - 2, z] = Terrain.FloorTile;
+                        Game.World.Covers[x1 + (i + 1) * 4, y1 + (j + 1) * 4 - 2, z] = Cover.NoCover;
+                    }
+                    if (j < 2 && !down)
+                    {
+                        Game.World.Terrains[x1 + (i + 1) * 4 - 2, y1 + (j + 1) * 4, z] = Terrain.FloorTile;
+                        Game.World.Covers[x1 + (i + 1) * 4 - 2, y1 + (j + 1) * 4, z] = Cover.NoCover;
+                    }
+                }
+            }
+
             // smooth out the nearby slopes
             for (int i = -1; i <= +1; i++)
             {
                 for (int j = -1; j <= +1; j++)
                 {
-                    if (Game.World.Terrains[x + i, y + j, z] == Terrain.UpSlopeTile)
+                    int x2 = x0 - (hallwayLength + setPieceRadius) * dx;
+                    int y2 = y0 - (hallwayLength + setPieceRadius) * dy;
+                    if (Game.World.Terrains[x2 + i, y2 + j, z] == Terrain.UpSlopeTile)
                     {
-                        Game.World.Terrains[x + i, y + j, z] = Terrain.FloorTile;
-                        Game.World.Terrains[x + i, y + j, z + 1] = Terrain.EmptyTile;
+                        Game.World.Terrains[x2 + i, y2 + j, z] = Terrain.FloorTile;
+                        Game.World.Terrains[x2 + i, y2 + j, z + 1] = Terrain.EmptyTile;
                     }
                 }
             }
@@ -95,16 +179,14 @@ namespace Hecatomb
 
         public void PlaceDwarfLair()
         {
-            int maxTries = 500;
+            int maxTries = 1000;
             for (int i = 0; i < maxTries; i++)
             {
                 if (FindHillSide())
                 {
-                    Debug.WriteLine($"placed a dwarf lair after {i + 1} tries");
                     return;
                 }
             }
-            Debug.WriteLine("failed to place a dwarf lair");
         }
     }
 }
