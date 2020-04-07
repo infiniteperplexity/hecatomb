122fe0bf317ad33af68fefe39dc27467a5467536
infiniteperplexity <infinite.perplexity@gmail.com>
2020-04-07 16:02:48 -0700

    got a csharp8 project going it looks like

---
+++ csharp/Hecatomb8/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hecatomb8
+{
+    class Foo
+    {
+        public void Baz()
+        {
+
+        }
+    }
+
+    class Bar
+    {
+        public void Test()
+        {
+            var foo = new Foo();
+            foo!.Baz();
+        }
+    }
+}
